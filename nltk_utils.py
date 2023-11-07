9083381b022abaf9ad7bfdf1254d7b44c5df5572
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2023-11-06 21:47:42 -0300

    feature - chatbot finalizado

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,21 +1,49 @@
-import nltk;
 import numpy as np
-#nltk.download('punkt')
+import nltk
+# nltk.download('punkt')
+# nltk.download('stopwords')
+stopwords = nltk.corpus.stopwords.words('portuguese')
+
+# nltk.download('mac_morpho')
+nltk.corpus.mac_morpho.words()
+
 from nltk.stem.porter import PorterStemmer
-stemer = PorterStemmer()
+stemmer = PorterStemmer()
 
 def tokenize(sentence):
-    return nltk.word_tokenize(sentence)
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
+    return nltk.word_tokenize(sentence, language='portuguese')
+
 
 def stem(word):
-    return stemer.stem(word.lower())
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
+    return stemmer.stem(word.lower())
 
-def bag_of_words(tokenized_sentence, all_words):
-    tokenized_sentence = [stem(word) for word in tokenized_sentence]
 
-    bag = np.zeros(len(all_words), dtype = np.float32)
-    for index, word in enumerate(all_words):
-        if word in tokenized_sentence:
-            bag[index] = 1.0
-    
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
     return bag
--- train.py
+++ train.py
@@ -1,100 +1,129 @@
-import json
-from nltk_utils import tokenize, stem, bag_of_words
-import re
 import numpy as np
+import random
+import json
 
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
-with open('intents.json', 'r') as f:
+with open('intents.json', 'r', encoding='utf8') as f:
     intents = json.load(f)
 
 all_words = []
 tags = []
 xy = []
-
-#coleta de padrões e atribui rotulos a partir da tokenização
+# loop through each sentence in our intents patterns
 for intent in intents['intents']:
     tag = intent['tag']
+    # add to tag list
     tags.append(tag)
     for pattern in intent['patterns']:
-        wordsTokenized = tokenize(pattern)
-        all_words.extend(wordsTokenized)
-        xy.append((wordsTokenized, tag))
+        # tokenize each word in the sentence
+        w = tokenize(pattern)
+        # add to our words list
+        all_words.extend(w)
+        # add to xy pair
+        xy.append((w, tag))
 
-ignore_words = ['?', '!', '.', ',']
-
-#fazendo derivação
-all_words = [stem(wordsTokenized) for wordsTokenized in all_words if wordsTokenized not in ignore_words]
+# stem and lower each word
+ignore_words = ['?', '.', '!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# remove duplicates and sort
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-print(tags)
 
+print(len(xy), "patterns")
+print(len(tags), "tags:", tags)
+print(len(all_words), "palavras únicas stemmizadas:", all_words)
+
+# create training data
 X_train = []
 y_train = []
-
-#criação de bag_of_words
 for (pattern_sentence, tag) in xy:
+    # X: bag of words for each pattern_sentence
     bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
-
+    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
     label = tags.index(tag)
-    y_train.append(label) #CrossEntropyLoss -> 1 hot encoded
+    y_train.append(label)
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
+# Hyper-parameters 
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+print(input_size, output_size)
+
 class ChatDataset(Dataset):
+
     def __init__(self):
         self.n_samples = len(X_train)
         self.x_data = X_train
         self.y_data = y_train
 
-    #dataset[index]
+    # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
-    
+
+    # we can call len(dataset) to return the size
     def __len__(self):
         return self.n_samples
 
-
-#Hiperparâmetros
-num_epochs = 1000
-batch_size = 8
-learning_rate = 0.001
-input_size = len(X_train[0]) #bag of words que foi criado -> mesmo que all_words
-hidden_size = 8
-output_size = len(tags) #n° de diferentes classes e tags
-
 dataset = ChatDataset()
-train_loader = DataLoader(dataset = dataset, batch_size = batch_size, shuffle = True, num_workers = 0) #para iterar automaticamente em cima disso
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') #verifica se tem GPU disponível
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
-#perda e otimizador
+# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-#carregar treinamento
+# Train the model
 for epoch in range(num_epochs):
-    for(words, labels) in train_loader:
+    for (words, labels) in train_loader:
         words = words.to(device)
         labels = labels.to(dtype=torch.long).to(device)
-
-        #forward
+        
+        # Forward pass
         outputs = model(words)
+        # if y would be one-hot, we must apply
+        # labels = torch.max(labels, 1)[1]
         loss = criterion(outputs, labels)
-
-        #backward and optimizer step
+        
+        # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-    if(epoch + 1) % 100 == 0:
-        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
         
-print(f'final loss, loss={loss.item():.4f}')
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+
+print(f'final loss: {loss.item():.4f}')
+
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
